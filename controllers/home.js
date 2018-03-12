4849761e6d5e1c053c8e06047e893f261ad13d8c
Louise van den Berg <louisevdb84@gmail.com>
2018-03-12 21:01:31 +0200

    Initial Commit

---
+++ controllers/home.js
@@ -0,0 +1,54 @@
+const handleUpcomingMoviesGet = (req, res, fetch, apiKey) => {
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
+        .then(response => response.json())
+        .then(movies => res.json(movies.results))
+        .catch(err => res.status(400).json('Error getting upcoming movies'));    
+}
+
+const handleTopRatedGet = (req, res, fetch, apiKey) => {
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+        .then(response=>response.json())       
+        .then(movies => res.json(movies.results))  
+        .catch(err => res.status(400).json('Error getting top rated movies'));        
+}
+
+const handlePopularGet = (req, res, fetch, apiKey) => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+        .then(response=>response.json())       
+        .then(movies => res.json(movies.results))  
+        .catch(err => res.status(400).json('Error getting popular movies'));        
+}
+
+const handleNowPlayingGet = (req, res, fetch, apiKey) => {
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`)
+        .then(response=>response.json())       
+        .then(movies => res.json(movies.results))  
+        .catch(err => res.status(400).json('Error getting now playing movies'));        
+}
+
+const handleSearchPost = (req, res, fetch, apiKey) => {
+    
+    // fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=the+avengers`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${req.body.searchMovies}`)
+        .then(response=>response.json())       
+        .then(movies => res.json(movies.results))  
+        .catch(err => res.status(400).json('Error getting now playing movies'));        
+}
+
+const handleGenreGet = (req, res, fetch, apiKey) => {
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+        .then(response=>response.json())       
+        .then(movies => res.json(movies))  
+        .catch(err => res.status(400).json('Error getting now playing movies'));        
+}
+
+
+module.exports = {
+    handleUpcomingMoviesGet,
+    handleTopRatedGet,
+    handlePopularGet,
+    handleNowPlayingGet,
+    handleSearchPost,
+    handleGenreGet
+    
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const fetch = require('node-fetch');
+const home = require('./controllers/home');
+
+const app = express();
+const apiKey = "871feeb0aba09430c9465b40bcb07317";
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/upcomingMovies', (req, res) => { home.handleUpcomingMoviesGet(req, res, fetch, apiKey) });
+app.get('/topRated', (req, res) => { home.handleTopRatedGet(req, res, fetch, apiKey) });
+app.get('/popular', (req, res) => { home.handlePopularGet(req, res, fetch, apiKey) });
+app.get('/nowPlaying', (req, res) => { home.handleNowPlayingGet(req, res, fetch, apiKey) });
+app.post('/search', (req, res) => { home.handleSearchPost(req, res, fetch, apiKey) });
+app.get('/genres', (req, res) => { home.handleGenreGet(req, res, fetch, apiKey) });
+
+app.listen(process.env.PORT || 3001, () => console.log('App is running on port 3001'))
+
+
